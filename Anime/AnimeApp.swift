362a330715fc14ef12ca4b1349d0f2c630faacdd
Riley Williams <williams.riley@icloud.com>
2021-01-13 16:20:44 -0500

    Initial Commit

---
+++ Anime/AnimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeApp.swift
+//  Anime
+//
+//  Created by Riley Williams on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
